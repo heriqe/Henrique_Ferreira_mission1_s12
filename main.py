e4e3c3469f72e9d33fff17c4895630b773784c8f
Henrique Ferreira da Silva <hefesi93@gmail.com>
2024-05-29 17:58:39 -0300

    Inicializar projeto

---
+++ main.py
@@ -0,0 +1,11 @@
+def menu_principal():
+    print("\n**Calculadora de Conversão de Unidades**")
+    print("1. Converter Comprimento")
+    print("3. Sair")
+    opcao=input("Digite a opção desejada:  ")
+    return opcao
+def sair():
+    print("O programa está se encerrando")
+escolha=menu_principal
+if escolha=="3":
+    sair()
