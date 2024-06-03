51f085ecce7d5f91dadc98a08f0ce5bd735d74b4
Henrique Ferreira da Silva <hefesi93@gmail.com>
2024-06-03 15:42:28 -0300

    Cria função FahrenheitParacelsius

--- conversor_temperaturas.py
+++ conversor_temperaturas.py
@@ -1,5 +1,7 @@
 class Conversor_Temperaturas:
     def __init__(self) -> None:
         pass
-    def celsiusParaFahrenheit(celsius):
+    def CelsiusParaFahrenheit(celsius):
         return (celsius * 9/5) + 32
+    def FahrenheitParaCelsius(Fahrenheit):
+        return (Fahrenheit - 32) * 5/9
