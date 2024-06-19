221ada6d73a1b254b4fd006b4dc45f03d4b1e54c
Henrique Ferreira da Silva <hefesi93@gmail.com>
2024-06-19 18:16:38 -0300

    O código e funcionamento

--- conversor_medidas.py
+++ conversor_medidas.py
@@ -1,24 +1,8 @@
 class ConversorMedidas:
-    def __init__(self) -> None:
+    def __init__(self):
         pass
-    def centimetrosParaMetros(centimetros):
-        return centimetros / 100
-    def MetrosParaCentimetros(Metros):
-        return Metros * 100
-    print("*Conversor de Medidas*")
-    print("1. Converter de centímetros para metros")
-    print("2. Converter de metros para centímetros")
-    while True:
-        escolha = input("Escolha uma opção: ")
-        if escolha == "1":
-         centimetros = float(input("Digite o valor em centímetros: "))
-         metros = centimetrosParaMetros(centimetros)
-         print(f"{centimetros} centímetros é igual a {metros} metros")
-        elif escolha == "2":
-            metros = float(input("Digite o valor em metros: "))
-            centimetros = MetrosParaCentimetros(metros)
-            print(f"{metros} metros é igual a {centimetros} centímetros")
-        elif escolha=="sair":
-            break
-        else:
-            print("Opção inválida. Por favor, tente novamente.")
+    def centimetrosParaMetros(valor):
+        return valor / 100
+    def MetrosParaCentimetros(valor):
+        return valor * 100
+  
--- main.py
+++ main.py
@@ -1,21 +1,27 @@
-def sair():
-    print("Encerrando o programa...")
-
+from conversor_medidas import ConversorMedidas
+from conversor_temperaturas import Conversor_Temperaturas
 def menu_principal():
     while True:
         print("\n*Calculadora de Conversão de Unidades*")
-        print("1. Conversor de Medidas")
-        print("2. Conversor de Temperaturas")   
-        print("3. Sair")
-        opção = input("Digite a opção desejada: ")
-        if opção == "1":
-            from conversor_medidas import ConversorMedidas
-        elif opção == "2":
-            from conversor_temperaturas import Conversor_Temperaturas
-        elif opção == "3":
-            sair()
+        print("1. Converter de centímetros para metros")
+        print("2. Converter de metros para centímetros")
+        print("3. Converter de celsius para fahrenheit")
+        print("4. Converter de fahrenheit para celsius")   
+        print("5. Sair")
+        opção = str(input("Digite a opção desejada: "))
+        if opção == "5":
             break
+        valor = float(input("Digite o seu valor:  "))
+        if opção == "1":
+            valor = ConversorMedidas.centimetrosParaMetros(valor)
+        elif opção == "2":
+            valor = ConversorMedidas.MetrosParaCentimetros(valor)
+        elif opção == "3":
+            valor= Conversor_Temperaturas.CelsiusParaFahrenheit(valor)
+        elif opção == "4":
+            valor= Conversor_Temperaturas.FahrenheitParaCelsius(valor)
         else:
             print("Opção inválida. Tente novamente.")
+        print('Resultado:   ',valor)
 
 menu_principal()
